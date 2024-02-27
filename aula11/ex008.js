b68044ebc2ae130d9b72afaf6dbffd98ba45a231
DioneSilviodosReis <100045795+DioneSilviodosReis@users.noreply.github.com>
2024-02-26 21:46:25 -0300

    aula 26/02 JS

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+
+
+var vel = 120
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if(vel > 60)
+{
+    console.log('Voce foi multado')
+}
+else
+{
+    console.log('Dirija com cuidado')
+}
